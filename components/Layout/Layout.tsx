0bc1ff0a18e51375f9618baf7d71552fe16f6a99
Sina Zarrinpour <sina.zarinpoor@yahoo.com>
2020-10-28 23:43:35 +0330

    sina

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -14,6 +14,7 @@ export default function Layout({ children, title = '' }: Props) {
       <title>{"Trustore | " + title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     </Head>
     <Header />
